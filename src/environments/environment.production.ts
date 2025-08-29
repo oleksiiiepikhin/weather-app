a63fa2a94f63b9e8bab2465dd553129df446d4a0
oleksiiiepikhin <oleksii.iepikhin@gmail.com>
2025-08-29 23:36:47 +0200

    Put @ngxs dependencies + env variables

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    openWeather: {
+      apiKey: 'ef739cedbba426ddff320ccd9de28006',
+      geoBase: 'https://api.openweathermap.org/geo/1.0',
+      currentBase: 'https://api.openweathermap.org/data/2.5/weather',
+      forecastBase: 'https://api.openweathermap.org/data/2.5/forecast',
+    },
+};
